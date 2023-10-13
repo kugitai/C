890b59c77c5a8377dee786f1bce32c85d8b43b82
kugitai <98938330+kugitai@users.noreply.github.com>
2023-10-13 15:23:27 +0900

    無限ループの削除（バグの発生源の為）

--- cal.c
+++ cal.c
@@ -19,7 +19,6 @@ int div(float a,float b){     //割り算の関数
 
 int main(void){
     int foo; //終了するか否かの判定のための仮の変数
-    while(1) {
     int n1,n2;
     int sw;
     printf("1つ目の数:"); scanf("%d",&n1);
@@ -31,11 +30,7 @@ int main(void){
         case 2 : printf("計算結果は%dです\n",sub(n1,n2)); 
         case 3 : printf("計算結果は%dです\n",mul(n1,n2));
         case 4 : printf("計算結果は%dです\n",div((float)n1,(float)n2));}
-    puts("終了しますか？(yesなら0を押してエンター,noならそのままエンター)"); 
-    scanf("%d",&foo);
-    if(foo == 0){
-        break;
-    }
-    }
+    //puts("終了しますか？(yesなら0を押してエンター,noならそのままエンター)"); 
+    //scanf("%d",&foo);
     return 0;
 }
