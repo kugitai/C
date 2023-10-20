49d8965baa3426904cf6ce35d306b9156dbdb64d
kugitai <98938330+kugitai@users.noreply.github.com>
2023-10-20 09:37:47 +0900

    再入力を受け付けるようにした（無入力だと弾くようにした）

--- cal.c
+++ cal.c
@@ -19,21 +19,28 @@ int div(float a,float b){     //割り算の関数
 
 int main(void){
     int foo; //終了するか否かの判定のための仮の変数
-    char buff[100];
-    int n1,n2;
+    char buff[100]; //入力を受け取るための変数
+    int n1 = 0,n2 = 0;
     int sw;
-    printf("1つ目の数:");
-    fgets(buff,100,stdin);
-    sscanf(buff,"%d",&n1);
-    printf("2つ目の数:");
-    fgets(buff,100,stdin);
-    sscanf(buff,"%d",&n2);
+while(1){
+    while(n1 == 0 || n2 == 0)
+        {
+        printf("1つ目の数:");
+        fgets(buff,100,stdin);
+        sscanf(buff,"%d",&n1);
+        printf("2つ目の数:");
+        fgets(buff,100,stdin);
+        sscanf(buff,"%d",&n2);
+        }
     puts("どの計算をしますか?");
     printf("足し算:1 引き算:2 掛け算:3 割り算:4  "); scanf("%d",&sw);
-    switch (sw) {
+    switch (sw)
+        {
         case 1 : printf("計算結果は%dです\n",add(n1,n2)); break;
         case 2 : printf("計算結果は%dです\n",sub(n1,n2)); break; 
         case 3 : printf("計算結果は%dです\n",mul(n1,n2)); break;
-        case 4 : printf("計算結果は%dです\n",div((float)n1,(float)n2));}
+        case 4 : printf("計算結果は%dです\n",div((float)n1,(float)n2));
+        }
+    }
     return 0;
 }
