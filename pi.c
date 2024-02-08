0928c95a3040417f00f27caa4ba40ce0d5bb54e9
kugitai <98938330+kugitai@users.noreply.github.com>
2024-02-08 14:11:41 +0900

    見やすく変更

--- pi.c
+++ pi.c
@@ -1,21 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void){
-    int inpoint = 0;//打った点のうち円の中に入ったもの
-    int allpoint;//全ての点
-    int i;//カウンター
-    float x;//打った点のX座標
-    float y;//打った点のY座標
-    float pi;
+    int inpoint = 0,allpoint;//打った点のうち円の中に入ったものと全ての点
+    float x,y,pi; //打った点のx座標とy座標と結果
     srand(time(NULL));
 
     puts("何個の点をおきますか？(入力したらエンターを押してください)");
 
     scanf("%d",&allpoint);
     
-    for(i = 0;i < allpoint;i++){
+    for(int i = 0;i < allpoint;i++){
         x = (float)rand()/RAND_MAX;
         y = (float)rand()/RAND_MAX;
         if(x*x+y*y <= 1.0){
