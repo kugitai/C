812cb028774cd41878ee36f4178836f5aacd0ab6
kugitai <98938330+kugitai@users.noreply.github.com>
2025-01-16 11:20:38 +0900

    内容改善
    定数の追加
    バグ
    適切に再起動できない

--- cal.c
+++ cal.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#define TRUE 1
+#define BUFF_SIZE 1024
 
 int add(int a,int b){     //足し算の関数
     int c = a + b;
@@ -19,27 +21,35 @@ int div(float a,float b){     //割り算の関数
 
 int main(void){
     int foo; //終了するか否かの判定のための仮の変数
-    char buff[100]; //入力を受け取るための変数
+    char buff[BUFF_SIZE]; //入力を受け取るための変数
     int n1,n2;
     int sw;
-do{
-    printf("1つ目の数:");
-    fgets(buff,100,stdin);
-    sscanf(buff,"%d",&n1);
-    printf("2つ目の数:");
-    fgets(buff,100,stdin);
-    sscanf(buff,"%d",&n2);
-    puts("どの計算をしますか?");
-    printf("足し算:1 引き算:2 掛け算:3 割り算:4  "); scanf("%d",&sw);
-    switch (sw)
+    while(TRUE){
+        printf("1つ目の数:");
+        fgets(buff,BUFF_SIZE,stdin);
+        sscanf(buff,"%d",&n1);
+        printf("2つ目の数:");
+        fgets(buff,BUFF_SIZE,stdin);
+        sscanf(buff,"%d",&n2);
+        puts("どの計算をしますか?");
+        printf("足し算:1 引き算:2 掛け算:3 割り算:4  "); 
+        scanf("%d",&sw);
+        switch (sw)
         {
-        case 1 : printf("計算結果は%dです\n",add(n1,n2)); break;
-        case 2 : printf("計算結果は%dです\n",sub(n1,n2)); break; 
-        case 3 : printf("計算結果は%dです\n",mul(n1,n2)); break;
-        case 4 : printf("計算結果は%dです\n",div((float)n1,(float)n2));
+            case 1 : printf("計算結果は%dです\n",add(n1,n2)); break;
+            case 2 : printf("計算結果は%dです\n",sub(n1,n2)); break; 
+            case 3 : printf("計算結果は%dです\n",mul(n1,n2)); break;
+            case 4 : printf("計算結果は%dです\n",div((float)n1,(float)n2)); break;
+            default: puts("エラーです"); break;
         }
-    puts("終了しますか？(Y/n)");
-    scanf("%s",&foo);
-    }while(strcmp(foo,"Y") != 0);
+        puts("終了しますか？");
+        puts("0:終了 1:続ける");
+        scanf("%d",&foo);
+        if(foo == 0){
+            break;
+        }
+    }
+    puts("終了します");
     return 0;
 }
+
